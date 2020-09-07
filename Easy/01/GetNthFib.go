16171b9d3c840c8e7ae83a8a0513d3a4747c4501
lee-hen <leehenry303@gmail.com>
2020-09-07 21:54:01 +0900

    first commit with two easy questions

---
+++ Easy/01/GetNthFib.go
@@ -0,0 +1,64 @@
+package main
+
+func GetNthFib(n int) int {
+	return help(n, map[int]int{1: 0, 2: 1})
+}
+
+func help(n int, memoize map[int]int) int {
+	if val, ok := memoize[n]; ok {
+		return val
+	}
+
+	memoize[n] = help(n-2, memoize) + help(n-1, memoize)
+	return memoize[n]
+}
+
+func GetNthFib3(n int) int {
+
+	lastTwo := []int{0, 0}
+	var nextFib int
+
+	for i := 1; i <= n; i++ {
+		if i == 2 {
+			lastTwo[1] = 1
+		}
+		lastTwo = []int{lastTwo[1], nextFib}
+		nextFib = lastTwo[1] + lastTwo[0]
+	}
+
+	return nextFib
+}
+
+// My solution
+
+func GetNthFib1(n int) int {
+	if n == 1 {
+		return 0
+	}
+
+	if n == 2 {
+		return 1
+	}
+
+	return GetNthFib1(n-1) + GetNthFib1(n-2)
+}
+
+func GetNthFib2(n int) int {
+	var prevFirst int
+	var prevLast int
+	var this int
+
+	for i := 1; i <= n; i++ {
+
+		if i == 2 {
+			prevLast = 1
+		}
+
+		prevFirst = prevLast
+		prevLast = this
+
+		this = prevFirst + prevLast
+	}
+
+	return this
+}
---
+++ Easy/02/TwoNumberSum.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"sort"
+)
+
+func TwoNumberSum(array []int, target int) []int {
+	sort.Ints(array)
+	left, right := 0, len(array)-1
+	for left < right {
+		sum := array[left] + array[right]
+		if sum == target {
+			return []int{array[left], array[right]}
+		} else if sum < target {
+			left++
+		} else {
+			right--
+		}
+	}
+	return []int{}
+}
+
+// My solution
+func TwoNumberSum1(array []int, target int) []int {
+
+	for i, x := range array {
+		for _, y := range array[i+1:] {
+			if x+y == target {
+				return []int{x, y}
+			}
+		}
+	}
+	return []int{}
+}
+
+func TwoNumberSum2(array []int, target int) []int {
+	memorize := make(map[int]struct{})
+	for _, x := range array {
+		memorize[x] = struct{}{}
+	}
+
+	for _, y := range array {
+		x := target - y
+		if x != y {
+			if _, ok := memorize[x]; ok {
+				return []int{target - y, y}
+			}
+		}
+	}
+	return []int{}
+}
