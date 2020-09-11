583b75d2159fd163ecaf0834b9ca6ee83cba6fc9
lee-hen <leehenry303@gmail.com>
2020-09-11 10:58:49 +0900

    update insertion sort

--- Easy/11-13/InsertionSort.go
+++ Easy/11-13/InsertionSort.go
@@ -1,7 +1,16 @@
 package main
 
-// My solution
 func InsertionSort(array []int) []int {
+	for i := range array {
+		for j := i; j > 0 && array[j] < array[j-1]; j-- {
+			array[j], array[j-1] = array[j-1], array[j]
+		}
+	}
+	return array
+}
+
+// My solution
+func InsertionSort1(array []int) []int {
 	for i := 1; i < len(array); i++ {
 		for j := 0; j < i+1; j++ {
 			if array[j] > array[i] {
