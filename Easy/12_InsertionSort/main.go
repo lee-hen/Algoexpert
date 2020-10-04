5f163d43d0371c47290a4351fee4999a995d6694
lee-hen330 <leehenry303@gmail.com>
2020-10-04 20:08:48 +0900

    Fix separate files mistakes

--- Easy/12_InsertionSort/main.go
+++ Easy/12_InsertionSort/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	expected := []int{2, 3, 5, 5, 6, 8, 9}
-	output = InsertionSort([]int{8, 5, 2, 9, 5, 6, 3})
+	output := InsertionSort([]int{8, 5, 2, 9, 5, 6, 3})
 	fmt.Println(output)
 	fmt.Println(expected)
 }
