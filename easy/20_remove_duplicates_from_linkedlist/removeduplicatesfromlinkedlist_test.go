67ed55bf8b6160dbda9b08a21a78d688473f0e42
lee-hen <leehenry303@gmail.com>
2021-04-03 10:14:55 +0900

    add remove_duplicates_from_linkedlist

---
+++ easy/20_remove_duplicates_from_linkedlist/removeduplicatesfromlinkedlist_test.go
@@ -0,0 +1,17 @@
+package remove_duplicates_from_linkedlist
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestCase1(t *testing.T) {
+	actual := &LinkedList{Value: 1}
+	expected := &LinkedList{Value: 1}
+
+	actual.addMany([]int{1, 3, 4, 4, 4, 5, 6, 6})
+	expected.addMany([]int{3, 4, 5, 6})
+
+	actual.RmoveDuplicatesFromLinkedList()
+	require.Equal(t, expected.getValues(), actual.getValues())
+}
