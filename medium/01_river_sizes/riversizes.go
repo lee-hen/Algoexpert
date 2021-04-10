b9c582983e9ae9b31b9f0f8c19941c610920fc7a
lee-hen <leehenry303@gmail.com>
2021-04-10 10:55:41 +0900

    Add river sizes question and hint

--- medium/01_river_sizes/riversizes.go
+++ medium/01_river_sizes/riversizes.go
@@ -1,7 +1,77 @@
 package river_sizes
 
-// my corrected solution
+// Better solution but not understandable
+// O(wh) time | O(wh) space
 func RiverSizes(matrix [][]int) []int {
+	sizes := make([]int, 0)
+	visited := make([][]bool, len(matrix))
+
+	for i := range visited {
+		visited[i] = make([]bool, len(matrix[i]))
+	}
+
+	for i := range matrix {
+		for j := range matrix[i] {
+			if visited[i][j] {
+				continue
+			}
+
+			sizes = traverseNode(i, j, matrix, visited, sizes)
+		}
+	}
+	return sizes
+}
+
+func traverseNode(i, j int, matrix [][]int, visited [][]bool, sizes []int) []int {
+	currentRiverSize := 0
+	nodesToExplore := [][]int{{i, j}}
+
+	for len(nodesToExplore) > 0 {
+		currentNode := nodesToExplore[0]
+		nodesToExplore = nodesToExplore[1:]
+		i, j := currentNode[0], currentNode[1]
+
+		visited[i][j] = true
+		if matrix[i][j] == 0 {
+			continue
+		}
+
+		currentRiverSize += 1
+		unvisitedNeighbors := getUnvisitedNeighbors(i, j, matrix, visited)
+
+		for _, neighbor := range unvisitedNeighbors {
+			nodesToExplore = append(nodesToExplore, neighbor)
+		}
+	}
+
+	if currentRiverSize > 0 {
+		sizes = append(sizes, currentRiverSize)
+	}
+	return sizes
+}
+
+func getUnvisitedNeighbors(i, j int, matrix [][]int, visited [][]bool) [][]int {
+	unvisitedNeighbors := make([][]int, 0)
+
+	if i > 0 && !visited[i-1][j] {
+		unvisitedNeighbors = append(unvisitedNeighbors, []int{i - 1, j})
+	}
+	if i < len(matrix)-1 && !visited[i+1][j] {
+		unvisitedNeighbors = append(unvisitedNeighbors, []int{i + 1, j})
+	}
+	if j > 0 && !visited[i][j-1] {
+		unvisitedNeighbors = append(unvisitedNeighbors, []int{i, j - 1})
+	}
+	if j < len(matrix[0])-1 && !visited[i][j+1] {
+		unvisitedNeighbors = append(unvisitedNeighbors, []int{i, j + 1})
+	}
+
+	return unvisitedNeighbors
+}
+
+
+// my corrected solution
+func riverSizes(matrix [][]int) []int {
 	riverCoordinates := make([]map[int]struct{}, len(matrix))
 	for idxRow, matrixRow := range matrix {
 		var riverCoordinate = make(map[int]struct{})
@@ -89,7 +159,7 @@ func isVisited(riverCoordinates []map[int]struct{}, idxRow int, idxCol int) bool
 
 
 // my uncorrected solution
-func riverSizes(matrix [][]int) []int {
+func riverSizes1(matrix [][]int) []int {
 	riverCoordinates := make([]map[int]struct{}, len(matrix))
 	for idxRow, matrixRow := range matrix {
 		var riverCoordinate = make(map[int]struct{})
