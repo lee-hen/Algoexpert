3c00293cfe08627a272ee681ff5603233874f8d7
lee-hen <leehenry303@gmail.com>
2021-05-15 09:49:38 +0900

    complete medium questions

--- medium/52_sort_stack/sort_stack.go
+++ medium/52_sort_stack/sort_stack.go
@@ -1,5 +1,62 @@
 package sort_stack
 
 func SortStack(stack []int) []int {
-	return []int{}
+	if len(stack) == 0 {
+		return stack
+	}
+
+	top := stack[len(stack)-1]
+	stack = stack[:len(stack)-1]
+
+	SortStack(stack)
+	insertInSortedOrder(&stack, top)
+
+	return stack
+}
+
+func insertInSortedOrder(stack *[]int, value int) {
+	if len(*stack) == 0 || (*stack)[len(*stack)-1] <= value {
+		*stack = append(*stack, value)
+		return
+	}
+
+	top := (*stack)[len(*stack)-1]
+	*stack = (*stack)[:len(*stack)-1]
+
+	insertInSortedOrder(stack, value)
+	*stack = append(*stack, top)
+}
+
+func sortStack(stack []int) []int {
+	if len(stack) == 0 {
+		return stack
+	}
+
+	sortStackHelper(&stack)
+	return stack
+}
+
+func sortStackHelper(stack *[]int) {
+	top := (*stack)[len(*stack)-1]
+	*stack = (*stack)[:len(*stack)-1]
+
+	if len(*stack) == 0 {
+		*stack = append(*stack, top)
+		return
+	}
+
+	sortStackHelper(stack)
+
+	if (*stack)[len(*stack)-1] <= top {
+		*stack = append(*stack, top)
+	} else {
+		value := top
+
+		top = (*stack)[len(*stack)-1]
+		*stack = (*stack)[:len(*stack)-1]
+
+		*stack = append(*stack, value)
+		*stack = append(*stack, top)
+		sortStackHelper(stack)
+	}
 }
