c0f59bd0b1c2caa7f2cccf59d29c18cf928fdffd
lee-hen <leehenry303@gmail.com>
2021-05-19 22:22:55 +0900

    Add  SameBSTs solutions

--- hard/06_same_bsts/same_bsts_test.go
+++ hard/06_same_bsts/same_bsts_test.go
@@ -8,5 +8,5 @@ import (
 func TestCase1(t *testing.T) {
 	arrayOne := []int{10, 15, 8, 12, 94, 81, 5, 2, 11}
 	arrayTwo := []int{10, 8, 5, 15, 2, 12, 11, 94, 81}
-	require.Equal(t, SameBsts(arrayOne, arrayTwo), true)
+	require.Equal(t, SameBSTs(arrayOne, arrayTwo), true)
 }
