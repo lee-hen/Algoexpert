52cd23254f1f75d45e9aaf13583c8fe836444e25
lee-hen <leehenry303@gmail.com>
2021-06-04 16:44:39 +0900

    Add dijkstras_algorithm implementation

--- hard/17_dijkstras_algorithm/dijkstras_algorithm.go
+++ hard/17_dijkstras_algorithm/dijkstras_algorithm.go
@@ -1,5 +1,78 @@
 package dijkstras_algorithm
 
-func DijkstrasAlgorithm(start int, edges [][][]int) []int {
-	return nil
+import "math"
+
+//[[1, 7]],
+//[[2, 6], [3, 20], [4, 3]],
+//[[3, 14]],
+//[[4, 2]],
+//[],
+//[],
+
+func dijkstrasAlgorithm(start int, edges [][][]int) []int {
+	shortestPaths := make([]int, len(edges))
+	for i := range edges {
+		shortestPaths[i] = math.MaxInt32
+	}
+	shortestPaths[start] = 0
+	dijkstrasHelper(shortestPaths, edges)
+
+	return shortestPaths
+}
+
+func dijkstrasHelper(shortestPaths []int, edges [][][]int) {
+	visited := make(map[int]struct{})
+
+	for len(visited) < len(edges) {
+		currNode, currShortest := findNodeWithMinWeight(shortestPaths, visited)
+
+		if currShortest == math.MaxInt32 {
+			for i, shortestPath := range shortestPaths {
+				if shortestPath == math.MaxInt32 {
+					currNode = i
+					shortestPaths[currNode] = -1
+				}
+			}
+			break
+		}
+
+
+		edge := edges[currNode]
+
+		for _, pair := range edge {
+			dist := pair[0]
+			distance := pair[1]
+			shortestPaths[dist] = min(shortestPaths[dist], distance+currShortest)
+		}
+
+		visited[currNode] = struct{}{}
+	}
+}
+
+func findNodeWithMinWeight(shortestPaths []int, visited map[int]struct{}) (int, int) {
+	shortest := math.MaxInt32
+	var currNode int
+	for i, shortestPath := range shortestPaths {
+		if _, found := visited[i]; found {
+			continue
+		}
+
+		if shortestPath < shortest {
+			shortest = shortestPath
+			currNode = i
+		}
+	}
+
+	return currNode, shortest
+}
+
+
+func min(arg1 int, rest ...int) int {
+	curr := arg1
+	for _, num := range rest {
+		if num < curr {
+			curr = num
+		}
+	}
+	return curr
 }
--- hard/17_dijkstras_algorithm/dijkstras_algorithm_test.go
+++ hard/17_dijkstras_algorithm/dijkstras_algorithm_test.go
@@ -1,8 +1,9 @@
 package dijkstras_algorithm
 
 import (
-	"github.com/stretchr/testify/require"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestCase1(t *testing.T) {
@@ -16,6 +17,65 @@ func TestCase1(t *testing.T) {
 		{},
 	}
 	expected := []int{0, 7, 13, 27, 10, -1}
+	actual := DijkstrasAlgorithm(start, edges)
+	require.Equal(t, expected, actual)
+}
+
+func TestCase2(t *testing.T) {
+	start := 1
+	edges := [][][]int{
+		{{1, 2}},
+		{{0, 1}},
+		{{3, 1}},
+		{{2, 2}},
+	}
+	expected := []int{1, 0, -1, -1}
+	actual := DijkstrasAlgorithm(start, edges)
+	require.Equal(t, expected, actual)
+}
+
+func TestCase3(t *testing.T) {
+	start := 3
+	edges := [][][]int{
+		{
+			{1, 2},
+			{3, 3},
+			{4, 2},
+		},
+		{
+			{0, 1},
+			{6, 3},
+		},
+		{
+			{3, 9},
+		},
+		{
+			{0, 3},
+			{1, 4},
+			{4, 4},
+			{8, 7},
+		},
+		{
+			{0, 1},
+			{10, 3},
+		},
+		{
+			{7, 1},
+			{8, 4},
+		},
+		{
+			{8, 1},
+		},
+		{},
+		{
+			{7, 1},
+		},
+		{
+			{10, 2},
+		},
+		{},
+	}
+	expected := []int{3, 4, -1, 0, 4, -1, 7, 8, 7, -1, 7}
 	actual := DijkstrasAlgorithm(start, edges)
 	require.Equal(t, expected, actual)
 }
