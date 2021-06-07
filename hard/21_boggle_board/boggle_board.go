b52d1c3e64445e060ee869788f068443f6c95761
lee-hen <leehenry303@gmail.com>
2021-06-07 22:32:52 +0900

    finished boggle_board problem

--- hard/21_boggle_board/boggle_board.go
+++ hard/21_boggle_board/boggle_board.go
@@ -1,5 +1,115 @@
 package boggle_board
 
 func BoggleBoard(board [][]rune, words []string) []string {
-	return nil
+	trie := Trie{children: map[rune]Trie{}}
+	for _, word := range words {
+		trie.Add(word)
+	}
+
+	visited := make([][]bool, len(board))
+	for i := range visited {
+		visited[i] = make([]bool, len(board[i]))
+	}
+
+	finalWords := make(map[string]struct{})
+	for i := range board {
+		for j := range board[i] {
+			explore(i, j, board, trie, visited, finalWords)
+		}
+	}
+
+	result := make([]string, 0)
+	for word := range finalWords {
+		result = append(result, word)
+	}
+
+	return result
+}
+
+func explore(i, j int, board [][]rune, trie Trie, visited [][]bool, finalWords map[string]struct{}) {
+	if visited[i][j] {
+		return
+	}
+
+	letter := board[i][j]
+	if _, found := trie.children[letter]; !found {
+		return
+	}
+
+	visited[i][j] = true
+
+	trie = trie.children[letter]
+	if end, found := trie.children['*']; found {
+		finalWords[end.word] = struct{}{}
+	}
+
+	neighbors := getNeighbors(i, j, board)
+	for _, neighbor := range neighbors {
+		explore(neighbor[0], neighbor[1], board, trie, visited, finalWords)
+	}
+
+	visited[i][j] = false
+}
+
+func getNeighbors(i, j int, board [][]rune) [][]int {
+	neighbors := make([][]int, 0)
+
+	if i > 0 && j > 0 {
+		neighbors = append(neighbors, []int{i - 1, j - 1})
+	}
+
+	if i > 0 && j < len(board[0])-1 {
+		neighbors = append(neighbors, []int{i - 1, j + 1})
+	}
+
+	if i < len(board)-1 && j < len(board[0])-1 {
+		neighbors = append(neighbors, []int{i + 1, j + 1})
+	}
+
+	if i < len(board)-1 && j > 0 {
+		neighbors = append(neighbors, []int{i + 1, j - 1})
+	}
+
+	if i > 0 {
+		neighbors = append(neighbors, []int{i - 1, j})
+	}
+
+	if i < len(board)-1 {
+		neighbors = append(neighbors, []int{i + 1, j})
+	}
+
+	if j > 0 {
+		neighbors = append(neighbors, []int{i, j - 1})
+	}
+
+	if j < len(board[0])-1 {
+		neighbors = append(neighbors, []int{i, j + 1})
+	}
+
+	return neighbors
+}
+
+type Trie struct {
+	children map[rune]Trie
+
+	word string
+}
+
+func (t Trie) Add(word string) {
+	current := t
+
+	for _, letter := range word {
+		if _, found := current.children[letter]; !found {
+			current.children[letter] = Trie{
+				children: map[rune]Trie{},
+			}
+		}
+
+		current = current.children[letter]
+	}
+
+	current.children['*'] = Trie{
+		children: map[rune]Trie{},
+		word:     word,
+	}
 }
--- hard/21_boggle_board/boggle_board_test.go
+++ hard/21_boggle_board/boggle_board_test.go
@@ -20,6 +20,46 @@ func TestCase1(t *testing.T) {
 	}
 	words := []string{"this", "is", "not", "a", "simple", "boggle", "board", "test", "REPEATED", "NOTRE-PEATED"}
 	expected := []string{"this", "is", "a", "simple", "boggle", "board", "NOTRE-PEATED"}
+
 	output := BoggleBoard(board, words)
 	require.ElementsMatch(t, expected, output)
 }
+
+func TestCase2(t *testing.T) {
+	board := [][]rune{
+		{'f', 't', 'r', 'o', 'p', 'i', 'k', 'b', 'o'},
+		{'r', 'w', 'l', 'p', 'e', 'u', 'e', 'a', 'b'},
+		{'j', 'o', 't', 's', 'e', 'l', 'f', 'l', 'p'},
+		{'s', 'z', 'u', 't', 'h', 'u', 'o', 'p', 'i'},
+		{'k', 'a', 'e', 'g', 'n', 'd', 'r', 'g', 'a'},
+		{'h', 'n', 'l', 's', 'a', 't', 'e', 't', 'x'},
+	}
+
+	words := []string{"frozen", "rotten", "teleport", "city", "zutgatz", "kappa", "before", "rope", "obligate", "annoying"}
+	expected := []string{"rotten", "obligate", "before", "frozen", "kappa",  "rope",  "teleport"}
+	output := BoggleBoard(board, words)
+	require.ElementsMatch(t, expected, output)
+}
+
+
+func TestCase3(t *testing.T) {
+	board := [][]rune{
+		{'c', 'o', 'm'},
+		{'r', 'p', 'l'},
+		{'c', 'i', 't'},
+		{'o', 'a', 'e'},
+		{'f', 'o', 'd'},
+		{'z', 'r', 'b'},
+		{'g', 'i', 'a'},
+		{'o', 'a', 'g'},
+		{'f', 's', 'z'},
+		{'t', 'e', 'i'},
+		{'t', 'w', 'd'},
+	}
+	words := []string{"commerce", "complicated", "twisted", "zigzag", "comma", "foobar", "baz", "there"}
+	expected := []string{"complicated", "foobar", "twisted", "zigzag"}
+
+	output := BoggleBoard(board, words)
+	require.ElementsMatch(t, expected, output)
+}
+
