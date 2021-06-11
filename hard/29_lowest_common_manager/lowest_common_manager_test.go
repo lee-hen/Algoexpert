4c723c25bcb053d2a5843ae135e109815635ad65
lee-hen <leehenry303@gmail.com>
2021-06-11 18:27:33 +0900

    Add 29-30 hard problems

--- hard/29_lowest_common_manager/lowest_common_manager_test.go
+++ hard/29_lowest_common_manager/lowest_common_manager_test.go
@@ -2,9 +2,9 @@ package lowest_common_manager
 
 import (
 	"github.com/stretchr/testify/require"
+
 	"testing"
 )
-
 
 func (chart *OrgChart) addDirectReports(reports ...*OrgChart) {
 	chart.DirectReports = append(chart.DirectReports, reports...)
@@ -28,5 +28,23 @@ func TestCase1(t *testing.T) {
 	orgCharts['C'].addDirectReports(orgCharts['F'], orgCharts['G'])
 	orgCharts['D'].addDirectReports(orgCharts['H'], orgCharts['I'])
 	lcm := GetLowestCommonManager(orgCharts['A'], orgCharts['E'], orgCharts['I'])
-	require.Equal(t, lcm, orgCharts['B'], lcm)
+	require.Equal(t, orgCharts['B'], lcm)
+}
+
+func TestCase2(t *testing.T) {
+	orgCharts := getOrgCharts()
+	orgCharts['A'].addDirectReports(orgCharts['B'], orgCharts['C'], orgCharts['D'], orgCharts['E'], orgCharts['F'])
+	orgCharts['B'].addDirectReports(orgCharts['G'], orgCharts['H'], orgCharts['I'])
+	orgCharts['C'].addDirectReports(orgCharts['J'])
+	orgCharts['D'].addDirectReports(orgCharts['K'], orgCharts['L'])
+	orgCharts['F'].addDirectReports(orgCharts['M'], orgCharts['N'])
+	orgCharts['H'].addDirectReports(orgCharts['O'], orgCharts['P'], orgCharts['Q'], orgCharts['R'])
+	orgCharts['K'].addDirectReports(orgCharts['S'])
+	orgCharts['P'].addDirectReports(orgCharts['T'], orgCharts['U'])
+	orgCharts['R'].addDirectReports(orgCharts['V'])
+	orgCharts['V'].addDirectReports(orgCharts['W'], orgCharts['X'], orgCharts['Y'])
+	orgCharts['X'].addDirectReports(orgCharts['Z'])
+
+	lcm := getLowestCommonManager(orgCharts['A'], orgCharts['T'], orgCharts['H'])
+	require.Equal(t, orgCharts['H'], lcm)
 }
---
+++ hard/30_interweaving_strings/interweaving_strings_test.go
@@ -0,0 +1,63 @@
+package interweaving_strings
+
+import (
+	"github.com/stretchr/testify/require"
+
+	"testing"
+)
+
+func TestCase1(t *testing.T) {
+	one := "algoexpert"
+	two := "your-dream-job"
+	three := "your-algodream-expertjob"
+	require.Equal(t, true, InterweavingStrings(one, two, three))
+}
+
+func TestCase2(t *testing.T) {
+	one := "abc"
+	two := "123"
+	three := "a1b2c3"
+	require.Equal(t, true, InterweavingStrings(one, two, three))
+}
+
+func TestCase3(t *testing.T) {
+	one := "a"
+	two := "b"
+	three := "ab"
+	require.Equal(t, true, InterweavingStrings(one, two, three))
+}
+
+func TestCase4(t *testing.T) {
+	one := "a"
+	two := "b"
+	three := "ac"
+	require.Equal(t, false, InterweavingStrings(one, two, three))
+}
+
+func TestCase5(t *testing.T) {
+	one := "aacaaaa"
+	two := "aaabaaa"
+	three := "aaaabacaaaaaaa"
+	require.Equal(t, true, InterweavingStrings(one, two, three))
+}
+
+func TestCase6(t *testing.T) {
+	one := "aacaaaa"
+	two := "aaabaaa"
+	three := "aaaacabaaaaaaa"
+	require.Equal(t, true, InterweavingStrings(one, two, three))
+}
+
+func TestCase7(t *testing.T) {
+	one := "algo"
+	two := "frog"
+	three := "fralgogo"
+	require.Equal(t, true, InterweavingStrings(one, two, three))
+}
+
+func TestCase8(t *testing.T) {
+	one := "aa"
+	two := "ac"
+	three := "aaaa"
+	require.Equal(t, false, InterweavingStrings(one, two, three))
+}
