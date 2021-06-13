2c9a9d1e63d3b7b05c15b50c914b21234c2a584a
lee-hen <leehenry303@gmail.com>
2021-06-13 20:52:45 +0900

    add easy to remember bst remove solution

--- medium/08_bst_construction/bst_test.go
+++ medium/08_bst_construction/bst_test.go
@@ -24,8 +24,23 @@ func  TestCase1(t *testing.T) {
 	require.True(t, root.Right.Left.Left.Value == 12)
 
 	root.Remove(10)
-	require.True(t, root.Contains(10) == false)
-	require.True(t, root.Value == 12)
 
 	require.True(t, root.Contains(15))
+	require.True(t, root.Value == 12)
+	require.True(t, root.Contains(10) == false)
+}
+
+func  TestCase2(t *testing.T) {
+	root := NewBST(1)
+	root.Insert(2)
+	root.Insert(3)
+	root.Insert(4)
+	root.Remove(1)
+	require.True(t, root.Contains(1) == false)
+	root.Remove(2)
+	require.True(t, root.Contains(2) == false)
+	root.Remove(3)
+	require.True(t, root.Contains(3) == false)
+	root.Remove(4)
+	require.True(t, root.Contains(4) == true)
 }
