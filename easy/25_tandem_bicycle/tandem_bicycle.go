8d49e0653429ebf9f04509838bc16d7e1e00fcff
lee-hen <leehenry303@gmail.com>
2021-06-13 17:47:36 +0900

    remove uncorrected solution

--- easy/25_tandem_bicycle/tandem_bicycle.go
+++ easy/25_tandem_bicycle/tandem_bicycle.go
@@ -25,25 +25,3 @@ func TandemBicycle(redShirtSpeeds []int, blueShirtSpeeds []int, fastest bool) in
 	}
 	return totalSpeed
 }
-
-
-//  5, 5, 3, 9, 2
-//	3, 6, 7, 2, 1
-// My not correct solution
-func tandemBicycle1(redShirtSpeeds []int, blueShirtSpeeds []int, fastest bool) int {
-	sort.Ints(redShirtSpeeds)
-	sort.Sort(sort.Reverse(sort.IntSlice(blueShirtSpeeds)))
-
-	var totalSpeed int
-	for idx := 0; idx < len(redShirtSpeeds); idx++ {
-		redShirtSpeed := redShirtSpeeds[idx]
-		blueShirtSpeed := blueShirtSpeeds[idx]
-
-		if fastest {
-			totalSpeed += int(math.Max(float64(redShirtSpeed), float64(blueShirtSpeed)))
-		} else {
-			totalSpeed += int(math.Min(float64(redShirtSpeed), float64(blueShirtSpeed)))
-		}
-	}
-	return totalSpeed
-}
