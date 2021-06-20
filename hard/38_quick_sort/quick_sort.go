8ea0c1d63af51052a2605482fe782bc613b8dd8d
lee-hen <leehenry303@gmail.com>
2021-06-21 00:04:47 +0900

    Refactoring quick sort solutions

--- hard/38_quick_sort/quick_sort.go
+++ hard/38_quick_sort/quick_sort.go
@@ -1,6 +1,44 @@
 package quick_sort
 
 func QuickSort(array []int) []int {
+	sort(0, len(array)-1, array)
+	return array
+}
+
+func sort(lo, hi int, array []int) {
+	if lo >= hi {
+		return
+	}
+
+	pivot := partition(lo, hi, array)
+	sort(lo, pivot-1, array)
+	sort(pivot+1, hi, array)
+
+	return
+}
+
+func partition(pivot, hi int, array []int) int {
+	i, j := pivot+1, hi
+
+	for j >= i {
+		if array[i] > array[pivot] && array[j] < array[pivot] {
+			array[i], array[j] = array[j], array[i]
+		}
+
+		if array[i] <= array[pivot] {
+			i++
+		}
+
+		if array[j] >= array[pivot] {
+			j--
+		}
+	}
+
+	array[pivot], array[j] = array[j], array[pivot]
+	return j
+}
+
+func quickSort(array []int) []int {
 	sortHelper(0, len(array)-1, array)
 	return array
 }
@@ -31,45 +69,6 @@ func sortHelper(startIdx, endIdx int, array []int) {
 	return
 }
 
-func quickSort(array []int) []int {
-	sort(0, len(array)-1, array)
-	return array
-}
-
-func sort(lo, hi int, array []int) {
-	if lo >= hi {
-		return
-	}
-
-	j := partition(lo, hi, array)
-	sort(lo, j-1, array)
-	sort(j+1, hi, array)
-	return
-}
-
-func partition(lo, hi int, array []int) int {
-	pivot := lo
-	i, j := lo+1, hi
-
-	for  {
-		for i < hi && array[i] <= array[pivot] {
-			i++
-		}
-
-		for j > lo && array[pivot] <= array[j] {
-			j--
-		}
-
-		if i >= j {
-			break
-		}
-		array[i], array[j] = array[j], array[i]
-	}
-
-	array[pivot], array[j] = array[j], array[pivot]
-	return j
-}
-
 func QuickSort1(array []int) []int {
 	return helper(array, 0, len(array)-1)
 }
@@ -94,6 +93,7 @@ func helper(array []int, start, end int) []int {
 			right -= 1
 		}
 	}
+
 	array[pivot], array[right] = array[right], array[pivot]
 
 	if right-1-start < end-(right+1) {
--- hard/38_quick_sort/quick_sort_test.go
+++ hard/38_quick_sort/quick_sort_test.go
@@ -8,12 +8,12 @@ import (
 
 func TestCase1(t *testing.T) {
 	expected := []int{2, 3, 5, 5, 6, 8, 9}
-	output := QuickSort([]int{8, 5, 2, 9, 5, 6, 3})
+	output := QuickSort([]int{8, 5, 2, 9, 5, 5, 3})
 	require.Equal(t, expected, output)
 }
 
 func TestCase2(t *testing.T) {
 	expected := []int{-10, -7, -7, -6, -6, -5, -5, -4, -4, -4, -2, -1, 1, 3, 5, 5, 6, 8, 8, 10}
-	output := quickSort([]int{-4, 5, 10, 8, -10, -6, -4, -2, -5, 3, 5, -4, -5, -1, 1, 6, -7, -6, -7, 8})
+	output := QuickSort([]int{-4, 5, 10, 8, -10, -6, -4, -2, -5, 3, 5, -4, -5, -1, 1, 6, -7, -6, -7, 8})
 	require.Equal(t, expected, output)
 }
