ab08f468ad9eacee2dbb07672debcf647d7ef203
lee-hen <leehenry303@gmail.com>
2021-07-22 22:42:43 +0900

    Add count_inversions solution

--- very_hard/32_count_inversions/count_inversions_test.go
+++ very_hard/32_count_inversions/count_inversions_test.go
@@ -7,8 +7,36 @@ import (
 )
 
 func TestCase1(t *testing.T) {
+	input := []int{8, 5, 2, 9, 5, 6, 3}
+	expected := 12
+	actual := CountInversions(input)
+	require.Equal(t, expected, actual)
+}
+
+func TestCase2(t *testing.T) {
 	input := []int{2, 3, 3, 1, 9, 5, 6}
 	expected := 5
+	actual := CountInversions(input)
+	require.Equal(t, expected, actual)
+}
+
+func TestCase3(t *testing.T) {
+	input := []int{1, 2, 3, 4, 5, 6, -1}
+	expected := 6
+	actual := CountInversions(input)
+	require.Equal(t, expected, actual)
+}
+
+func TestCase4(t *testing.T) {
+	input := []int{54, 1, 2, 3, 4}
+	expected := 4
+	actual := CountInversions(input)
+	require.Equal(t, expected, actual)
+}
+
+func TestCase5(t *testing.T) {
+	input := []int{1, 10, 2, 8, 3, 7, 4, 6, 5}
+	expected := 16
 	actual := CountInversions(input)
 	require.Equal(t, expected, actual)
 }
