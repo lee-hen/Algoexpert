52ae44d5e5d7dde838915569df92f03d709c46e6
lee-hen <leehenry303@gmail.com>
2021-07-23 19:02:30 +0900

    Add linked_list_palindrome solutions

--- very_hard/26_linked_list_palindrome/linked_list_palindrome_test.go
+++ very_hard/26_linked_list_palindrome/linked_list_palindrome_test.go
@@ -17,3 +17,16 @@ func  TestCase1(t *testing.T) {
 	actual := LinkedListPalindrome(head)
 	require.Equal(t, expected, actual)
 }
+
+func  TestCase2(t *testing.T) {
+	head := &LinkedList{Value: 6}
+	head.Next = &LinkedList{Value: 5}
+	head.Next.Next = &LinkedList{Value: 4}
+	head.Next.Next.Next = &LinkedList{Value: 3}
+	head.Next.Next.Next.Next = &LinkedList{Value: 4}
+	head.Next.Next.Next.Next.Next = &LinkedList{Value: 5}
+	head.Next.Next.Next.Next.Next.Next = &LinkedList{Value: 6}
+	expected := true
+	actual := LinkedListPalindrome(head)
+	require.Equal(t, expected, actual)
+}
