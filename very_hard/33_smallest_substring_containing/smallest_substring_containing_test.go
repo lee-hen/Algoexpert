062fd60f2a9d5cdd92c5eb11a621aba215214337
lee-hen <leehenry303@gmail.com>
2021-07-25 12:41:58 +0900

    add smallest_substring_containing solution

--- very_hard/33_smallest_substring_containing/smallest_substring_containing_test.go
+++ very_hard/33_smallest_substring_containing/smallest_substring_containing_test.go
@@ -10,7 +10,61 @@ func TestCase1(t *testing.T) {
 	bigString := "abcd$ef$axb$c$"
 	smallString := "$$abf"
 	expected := "f$axb$"
+	require.Equal(t, expected,
+		SmallestSubstringContaining(bigString, smallString),
+	)
+}
+
+func TestCase2(t *testing.T) {
+	bigString := "abcdefghijklmnopqrstuvwxyz"
+	smallString := "aajjttwwxxzz"
+	expected := ""
+	require.Equal(t, expected,
+		SmallestSubstringContaining(bigString, smallString),
+	)
+}
+
+func TestCase3(t *testing.T) {
+	bigString := "abzacdwejxjftghiwjtklmnopqrstuvwxyz"
+	smallString := "aajjttwwxxzz"
+	expected := "abzacdwejxjftghiwjtklmnopqrstuvwxyz"
+	require.Equal(t,expected,
+		SmallestSubstringContaining(bigString, smallString),
+	)
+}
+
+func TestCase4(t *testing.T) {
+	bigString := "a$fuu+afff+affaffa+a$Affab+a+a+$a$"
+	smallString := "a+$aaAaaaa$++"
+	expected := "affa+a$Affab+a+a+$a"
 	require.Equal(t,
-		SmallestSubstringContaining(bigString, smallString), expected,
+		expected, SmallestSubstringContaining(bigString, smallString),
+	)
+}
+
+func TestCase5(t *testing.T) {
+	bigString := "1456243561288281932363365412356789901!"
+	smallString := "123!"
+	expected := "2356789901!"
+	require.Equal(t,
+		expected, SmallestSubstringContaining(bigString, smallString),
+	)
+}
+
+func TestCase6(t *testing.T) {
+	bigString := "14562435612!88281932363365$412356789901"
+	smallString := "$123!"
+	expected := "!88281932363365$"
+	require.Equal(t,
+		expected, SmallestSubstringContaining(bigString, smallString),
+	)
+}
+
+func TestCase7(t *testing.T) {
+	bigString := "14562435612z!8828!193236!336!5$41!23!5!6789901#"
+	smallString := "#!2z"
+	expected := "z!8828!193236!336!5$41!23!5!6789901#"
+	require.Equal(t,
+		expected, SmallestSubstringContaining(bigString, smallString),
 	)
 }
