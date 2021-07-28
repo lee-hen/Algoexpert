7244cb6f66b2800a725b8b894596ac73ff369ba6
lee-hen <leehenry303@gmail.com>
2021-07-29 01:12:59 +0900

    Add two_edge_connected_graph solutions

--- very_hard/21_two_edge_connected_graph/two_edge_connected_graph.go
+++ very_hard/21_two_edge_connected_graph/two_edge_connected_graph.go
@@ -1,5 +1,118 @@
 package two_edge_connected_graph
 
+// TwoEdgeConnectedGraph
+// O(v + e) time | O(v) space - where v is the number of
+// vertices and e is the number of edges in the graph
 func TwoEdgeConnectedGraph(edges [][]int) bool {
-	return false
+	if len(edges) == 0 {
+		return true
+	}
+
+	arrivalTimes := make([]int, len(edges))
+	for i := range edges {
+		arrivalTimes[i] = -1
+	}
+	startVertex := 0
+
+	if getMinimumArrivalTimeOfAncestors(startVertex, -1, 0, arrivalTimes, edges) == -1 {
+		return false
+	}
+
+	return areAllVerticesVisited(arrivalTimes)
+}
+
+func areAllVerticesVisited(arrivalTimes []int) bool {
+	for _, time := range arrivalTimes {
+		if time == -1 {
+			return false
+		}
+	}
+	return true
+}
+
+func getMinimumArrivalTimeOfAncestors(currentVertex, parent, currentTime int, arrivalTimes []int, edges [][]int) int {
+	arrivalTimes[currentVertex] = currentTime
+
+	var minimumArrivalTime = currentTime
+
+	for _, destination := range edges[currentVertex] {
+		if arrivalTimes[destination] == -1 {
+			minimumArrivalTime = min(minimumArrivalTime,
+				getMinimumArrivalTimeOfAncestors(destination, currentVertex, currentTime+1, arrivalTimes, edges))
+		} else if destination != parent {
+			minimumArrivalTime = min(minimumArrivalTime, arrivalTimes[destination])
+		}
+	}
+
+	// A bridge was detected, which means the graph isn't two-edge-connected.
+	if minimumArrivalTime == currentTime && parent != -1 {
+		return -1
+	}
+
+	return minimumArrivalTime
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// twoEdgeConnectedGraph
+// my solution
+// not good
+func twoEdgeConnectedGraph(edges [][]int) bool {
+	if len(edges) == 1 && len(edges[0]) == 0 {
+		return true
+	}
+
+	visited := make([]bool, len(edges))
+	for i := range edges {
+		if len(edges[i]) <= 1 {
+			return false
+		}
+
+		if visited[i] {
+			continue
+		}
+
+		if !dfs(i, i, i-1,i-1, edges, visited) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func dfs(idx, start, previous, ancestor int, edges [][]int, visited []bool) bool {
+	if visited[edges[start][len(edges[start])-1]] {
+		return true
+	}
+
+	var comeBackThroughSuccessor bool
+
+	for i := 0; i < len(edges[idx]); i++ {
+		if edges[idx][i] == previous || idx == start && visited[edges[idx][i]]  {
+			continue
+		}
+
+		if ancestor != previous && ancestor == idx && edges[idx][i] == start {
+			return false
+		}
+
+		if edges[idx][i] == start || visited[edges[idx][i]] {
+			visited[start] = true
+			visited[ancestor] = true
+			visited[idx] = true
+			return true
+		}
+
+		if idx == start {
+			ancestor = edges[idx][i]
+		}
+
+		comeBackThroughSuccessor = dfs(edges[idx][i], start, idx, ancestor, edges, visited)
+	}
+	return comeBackThroughSuccessor
 }
