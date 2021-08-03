f3da230bafb4c9a6da22dd5596e052fb36428ce7
lee-hen <leehenry303@gmail.com>
2021-08-03 15:54:41 +0900

    Add Glob Matching Solutions

--- assessments/01_glob_matching/glob_matching_test.go
+++ assessments/01_glob_matching/glob_matching_test.go
@@ -12,3 +12,93 @@ func TestCase1(t *testing.T) {
 	output := GlobMatching(fileName, pattern)
 	require.True(t, output)
 }
+
+func TestCase2(t *testing.T) {
+	fileName := "abcdagvvvvag"
+	pattern := "*?ag"
+	output := GlobMatching(fileName, pattern)
+	require.True(t, output)
+}
+
+func TestCase4(t *testing.T) {
+	fileName := "abcdagvvgaavg"
+	pattern := "*?aa?g"
+	output := GlobMatching(fileName, pattern)
+	require.True(t, output)
+}
+
+func TestCase5(t *testing.T) {
+	fileName := "abcdefg"
+	pattern := "abcddefg"
+	output := GlobMatching(fileName, pattern)
+	require.False(t, output)
+}
+
+func TestCase6(t *testing.T) {
+	fileName := ""
+	pattern := ""
+	output := GlobMatching(fileName, pattern)
+	require.True(t, output)
+}
+
+func TestCase7(t *testing.T) {
+	fileName := ""
+	pattern := "?"
+	output := GlobMatching(fileName, pattern)
+	require.False(t, output)
+}
+
+func TestCase8(t *testing.T) {
+	fileName := "abcdefg"
+	pattern := "*c*cdefg"
+	output := GlobMatching(fileName, pattern)
+	require.False(t, output)
+}
+
+func TestCase3(t *testing.T) {
+	fileName := "abcdagvvvvavg"
+	pattern := "*?ag"
+	output := GlobMatching(fileName, pattern)
+	require.False(t, output)
+}
+
+func TestCase9(t *testing.T) {
+	fileName := "abcdefg"
+	pattern := "abcd*efg"
+	output := GlobMatching(fileName, pattern)
+	require.True(t, output)
+}
+
+func TestCase10(t *testing.T) {
+	fileName := "abcdefg"
+	pattern := "?"
+	output := globMatching(fileName, pattern)
+	require.False(t, output)
+}
+
+func TestCase11(t *testing.T) {
+	fileName := "abcdefg"
+	pattern := "*****a*?f*********g"
+	output := GlobMatching(fileName, pattern)
+	require.True(t, output)
+}
+
+func TestCase12(t *testing.T) {
+	fileName := ""
+	pattern := "***"
+	output := GlobMatching(fileName, pattern)
+	require.True(t, output)
+}
+func TestCase13(t *testing.T) {
+	fileName := "abcdefg"
+	pattern := ""
+	output := GlobMatching(fileName, pattern)
+	require.False(t, output)
+}
+
+func TestCase14(t *testing.T) {
+	fileName := ""
+	pattern := ""
+	output := GlobMatching(fileName, pattern)
+	require.True(t, output)
+}
