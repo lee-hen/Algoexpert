cf548942fe1916a23700e0f1cd0f1d862d761d94
lee-hen <leehenry303@gmail.com>
2021-08-08 23:46:37 +0900

    change test

--- assessments/14_reverse_alternating_k_nodes/reverse_alternating_k_nodes_test.go
+++ assessments/14_reverse_alternating_k_nodes/reverse_alternating_k_nodes_test.go
@@ -11,7 +11,7 @@ func TestCase1(t *testing.T) {
 	addMany(ll, []int{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14})
 	k := 3
 	expected := []int{3, 2, 1, 4, 5, 6, 9, 8, 7, 10, 11, 12, 14, 13}
-	actual := reverseAlternatingKNodes(ll, k).Values()
+	actual := ReverseAlternatingKNodes(ll, k).Values()
 	require.Equal(t, expected, actual)
 }
 
